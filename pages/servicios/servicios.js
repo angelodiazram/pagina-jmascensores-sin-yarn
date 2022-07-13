cd2d8471746d3b9499de3c07071660e28a68fc69
Angelo Diaz <angelodiazram@gmail.com>
2022-07-13 11:58:13 -0400

    footer casi terminado

--- pages/servicios/servicios.js
+++ pages/servicios/servicios.js
@@ -1,7 +1,15 @@
+
 //? navbar scroll:
 
-const navbar = document.querySelector('.navbar');
+const header = document.querySelector('.header');
+
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.nav-menu');
 
 window.addEventListener('scroll', ()=>{
-    navbar.classList.toggle('active', window.scrollY > 0);
+    header.classList.toggle('active', window.scrollY > 0);
 });
+
+navbarToggle.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('visible');
+})
