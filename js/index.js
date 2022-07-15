df723bd0383041e06d2440dfdcfefa39619b2d8d
Angelo Diaz <angelodiazram@gmail.com>
2022-07-15 12:48:54 -0400

    carrusel terminado

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 
-//? navbar scroll:
-
-const header = document.querySelector('.header');
-
-const navbarToggle = document.querySelector('.navbar-toggle');
-const navbarMenu = document.querySelector('.nav-menu');
+const header            = document.querySelector('.header');
+const navbarToggle      = document.querySelector('.navbar-toggle');
+const navbarMenu        = document.querySelector('.nav-menu');
+const carouselContainer = document.querySelector('.contenedor-grande');
+const carouselButton  = document.querySelectorAll('.punto');
 
 window.addEventListener('scroll', ()=>{
     header.classList.toggle('active', window.scrollY > 0);
@@ -13,3 +12,22 @@ window.addEventListener('scroll', ()=>{
 navbarToggle.addEventListener('click', ()=>{
     navbarMenu.classList.toggle('visible');
 })
+
+
+carouselButton.forEach( (button, i) => {
+    // console.log({cadaPunto, i});
+
+    carouselButton[i].addEventListener('click', ()=>{
+        
+        let position = i;
+        let operation = position * -25;
+
+        carouselContainer.style.transform = `translateX(${operation}%)`;
+
+        carouselButton.forEach((button, i)=>{
+            carouselButton[i].classList.remove('activo');
+        });
+
+        carouselButton[i].classList.add('activo');
+    });
+});
